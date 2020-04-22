d810a7ae67a420890cb944257a9eacea4a542d1e
Simon Liétar <simon@lietar.net>
2020-04-22 23:28:39 +0200

    Add ffmpeg to the dependency list

--- server/default.nix
+++ server/default.nix
@@ -1,15 +1,17 @@
-{ python37Packages }:
+{ ffmpeg_4, python37Packages }:
 python37Packages.buildPythonApplication {
   pname = "opsavideo";
   version = "0.0.1";
   src = ./.;
-  propagatedBuildInputs = with python37Packages; [
+  propagatedBuildInputs = (with python37Packages; [
     (python37Packages.callPackage ./parse-torrent-name.nix {})
     PyChromecast
     aiohttp
     lxml
     watchdog
     websockets
-  ];
+  ]);
+  buildInputs = [ ffmpeg_4 ];
+  nativeBuildInputs = [ ffmpeg_4 ];
   doCheck = false;
 }
